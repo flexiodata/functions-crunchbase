0acc28b995fdefdc1592b2992d5ae2084036ce2e
Aaron Williams <aaron@flex.io>
2019-09-25 13:52:56 -0500

    added initial functions; partial implementation

---
+++ people.py
@@ -0,0 +1,64 @@
+
+# ---
+# name: cb-people
+# deployed: true
+# title: Crunchbase People
+# description: Returns information about people satisfying a search term
+# params:
+# - name: query
+#   type: string
+#   description: Query string to use when searching for people
+#   required: true
+# examples:
+# - '"Steve Jobs"'
+# - '"Bill Gates"'
+# notes: |-
+#   See https://www.crunchbase.com/ for more information
+# ---
+
+
+import json
+import requests
+from cerberus import Validator
+from collections import OrderedDict
+
+CONST_CRUNCHBASE_API_CONNECTION_NAME = '' # TODO: fill out
+
+def flex_handler(flex):
+
+    # get the authorization token
+    auth_token = flex.connections[CONST_CRUNCHBASE_API_CONNECTION_NAME].description
+
+    # get the input
+    input = flex.input.read()
+    try:
+        input = json.loads(input)
+        if not isinstance(input, list): raise ValueError
+    except ValueError:
+        raise ValueError
+
+    # define the expected parameters and map the values to the parameter names
+    # based on the positions of the keys/values
+    params = OrderedDict()
+    params['query'] = {'required': True, 'type': 'string'}
+    input = dict(zip(params.keys(), input))
+
+    # validate the mapped input against the validator
+    v = Validator(params, allow_unknown = True)
+    input = v.validated(input)
+    if input is None:
+        raise ValueError
+
+    query = input['query']
+
+    # api reference: https://data.crunchbase.com/reference
+    url = 'https://api.crunchbase.com/v3.1/people'
+    url += '?user_key=' + auth_token
+    url += '&query=' + query
+
+    response = requests.get(url)
+    content = response.json()
+
+    flex.output.content_type = 'application/json'
+    flex.output.write(content)
+
